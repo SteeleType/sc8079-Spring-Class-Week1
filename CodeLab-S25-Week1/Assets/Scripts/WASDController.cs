7b2a2b83b9ddc317d86a61cb35eda495155fcb00
SteeleType <scitrone97@gmail.com>
2025-01-28 13:01:35 -0500

    Made a WASD controller and game.
    Updated my game to meet the homework requirements.

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -1,31 +1,72 @@
+using System;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class WASDController : MonoBehaviour
 {
     public KeyCode keyUp = KeyCode.W;
+    public KeyCode keyDown = KeyCode.S;
+    public KeyCode keyLeft = KeyCode.A;
+    public KeyCode keyRight = KeyCode.D;
 
     public Rigidbody rb;
 
     public float moveForce = 10f;
 
-    Vector3 testVec;
+    public Vector3 testVec;
+    public Vector3 adjacentVec;
+    public Vector3 startPos;
+    float speed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("START!!!");
+        
+        startPos = transform.position;
 
-        rb = null;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+    
+        if (other.CompareTag("KillZone"))
+        {
+            transform.position = startPos;
+            Debug.Log("HIT");
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+        if ((other.CompareTag("Zone")) && (gameObject.GetComponent<Rigidbody>().linearVelocity == Vector3.zero))
+        {
+            Debug.Log("SCORE");
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
+    
     {
-        Debug.Log("update...");
-
+        speed = rb.linearVelocity.magnitude;
         if (Input.GetKey(keyUp))
         {
             rb.AddForce(testVec * moveForce);
+     
+        }
+        if (Input.GetKey(keyDown))
+        {
+            rb.AddForce(testVec * -moveForce);
+        }
+        if (Input.GetKey(keyLeft))
+        {
+            rb.AddForce(adjacentVec * -moveForce);
+        }
+        if (Input.GetKey(keyRight))
+        {
+            rb.AddForce(adjacentVec * moveForce);
         }
     }
 }
